53a7f2ad40b61953da13915843b83cf7c92839e1
Jhervin Jimenez <jhervinjimenez03@gmail.com>
2025-11-11 16:37:43 +0800

    updated filess

--- Backend/server.js
+++ Backend/server.js
@@ -37,7 +37,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // For all other routes, serve the React index.html
-app.get(/.*/, (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
